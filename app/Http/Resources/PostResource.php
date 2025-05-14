7aaf3e76a4b6cfbd1a2eb782b9c1393d1c6bae2b
Diego Oruezabal <diegooruezabal@gmail.com>
2025-05-14 16:57:41 +0200

    Subir imágenes

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 class PostResource extends JsonResource
 {
@@ -20,6 +21,7 @@ class PostResource extends JsonResource
             'slug' => $this-> slug,
             'excerpt' => $this-> excerpt,
             'body' => $this-> body,
+            'image' => $this->image_path ? Storage::url($this->image_path) : 'https://thumbs.dreamstime.com/b/no-image-available-icon-flat-vector-no-image-available-icon-flat-vector-illustration-132482953.jpg',
             'is_published' => $this-> is_published,
             'published_at' => $this-> published_at,
             'user' => UserResource::make($this->whenLoaded('user')),
